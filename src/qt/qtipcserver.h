8064dd900382cad89f2e8fcb405813b57ae094ca
CurrExpert <35582143+currexpert@users.noreply.github.com>
2018-03-01 00:52:57 +0100

    xt3ch
    xt3- xt3ch

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define xt3ch-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "xt3chURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
